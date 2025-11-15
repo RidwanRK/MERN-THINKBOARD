1097b2257663ea1b32a57eeb96e91e9a19982301
Ridwan <ridwanraish2003@gmail.com>
2025-11-16 00:38:06 +0600

    added middleware for rate limit, updated API routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 app.listen(PORT, () => {

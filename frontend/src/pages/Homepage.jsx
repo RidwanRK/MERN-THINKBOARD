2483d68ad6dc1b73b723723b706e89d5a73c67a2
Ridwan <ridwanraish2003@gmail.com>
2025-11-17 00:33:40 +0600

    updated homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
+import NoteCard from '../components/NoteCard.jsx';
 
 const Homepage = () => {
   const [isRateLimited,setisRateLimited] = useState(false);
@@ -37,6 +38,19 @@ const Homepage = () => {
     <div className= "min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {/* {notes.length === 0 && !isRateLimited && <NotesNotFound />} */}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }

b95c234b0758e4513db272047136ea54e903f7e9
Ridwan <ridwanraish2003@gmail.com>
2025-11-16 20:49:17 +0600

    tailwind and daisyUI setup for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes } from 'react-router'
+import  Homepage from "./pages/Homepage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import toast from 'react-hot-toast';
+import { Route } from 'react-router'
 
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
   )
 }
